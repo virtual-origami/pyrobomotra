8e31e1f2215a8261afb1a3960b1159c01cc3bd90
Karthik Shenoy <she@biba.uni-bremen.de>
2021-09-23 01:24:41 +0530

    fix host address of health server

--- pyrobomotra/health/health.py
+++ pyrobomotra/health/health.py
@@ -1,9 +1,15 @@
 import asyncio, socket
+import logging
+import sys
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG,
+                    format='%(levelname)-8s [%(filename)s:%(lineno)d] %(message)s')
+logger = logging.getLogger("Robot:Model")
 
 
 class HealthServer:
     def __init__(self, config, event_loop):
-        self.host = config["host"]
+        self.host = ""
         self.port = int(config["port"])
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((self.host, self.port))
@@ -16,6 +22,7 @@ class HealthServer:
         loop = asyncio.get_event_loop()
         response = 'OK\n'
         await loop.sock_sendall(client, response.encode('utf8'))
+        logger.debug("sent health status")
         client.close()
 
     async def server_loop(self):
